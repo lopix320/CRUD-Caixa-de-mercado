1b431e27bab9e5222065067d43f03ddfcda6c5ff
Gabriel Lopes Pereira <54251267+lopix320@users.noreply.github.com>
2022-03-08 11:53:54 -0300

    Update DB.java
    remove comentário

--- ProjetoIntegrador/src/main/java/projetointegrador/DB.java
+++ ProjetoIntegrador/src/main/java/projetointegrador/DB.java
@@ -9,8 +9,7 @@ public class DB {
        // 1 - Carregar o Driver
        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
        
-       // 2 - Conectar no Banco
-       // jdbc:sqlserver://localhost:1433;databaseName=projeto_loja;user=sa;password=pedro154789;       
+       // 2 - Conectar no Banco     
        Connection conexao = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=caixa;user=sa;password=0502");
        
        // 3 - Retorna uma conexao para quem chamou
